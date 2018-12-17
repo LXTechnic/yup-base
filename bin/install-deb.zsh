f9a01a18171979f2a21860224ca677b90e5c7092
A.M <andares@outlook.com>
2018-12-17 21:27:07 +0800

    fix bug for install-deb

--- bin/install-deb.zsh
+++ bin/install-deb.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 main() {
     local file=${1##*/}
-    wget -c $1 -O /tmp/${file} -o /var/log/install-deb.log
+    wget -c $1 -O /tmp/${file} -o /tmp/install-deb.log
     sudo dpkg -i /tmp/${file}
     rm /tmp/${file}
-    >/var/log/install-deb.log < /dev/null
+    >/tmp/install-deb.log < /dev/null
 }
 
 main $*
