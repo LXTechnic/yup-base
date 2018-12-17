967155887b801b6c6fc01602bce280ad1197a54f
A.M <andares@outlook.com>
2018-12-17 20:02:13 +0800

    update

--- cmd/oh-my-style.zsh
+++ cmd/oh-my-style.zsh
@@ -1,3 +1,7 @@
 oh-my-style() {
-    echo 'oh my style'
+    local url=$1
+    local file=${1##*/}
+    if [[ ! -e ~/.oh-my-zsh/themes/$file ]] {
+        echo "$(curl -fsSL $url)" >~/.oh-my-zsh/themes/$file
+    }
 }
