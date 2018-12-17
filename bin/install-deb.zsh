72ad76beebf639c6a37933d8d4ebcacd7de4b4c8
A.M <andares@outlook.com>
2018-12-17 21:26:06 +0800

    update bin install-deb

--- bin/install-deb.zsh
+++ bin/install-deb.zsh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh
 main() {
     local file=${1##*/}
-    wget -c $1 -O /tmp/${file}
+    wget -c $1 -O /tmp/${file} -o /var/log/install-deb.log
     sudo dpkg -i /tmp/${file}
     rm /tmp/${file}
+    >/var/log/install-deb.log < /dev/null
 }
 
 main $*
