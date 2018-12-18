22c7ac356bbbfcff7ba4febbcaea9f3da5f8c925
A.M <andares@outlook.com>
2018-12-18 21:03:45 +0800

    add path for local/bin

--- init.zsh
+++ init.zsh
@@ -1,1 +1,9 @@
 #!/usr/bin/env zsh
+
+if [[ -d /home/worker/local/bin ]] {
+    export PATH="/home/worker/local/bin:$PATH"
+}
+if [[ -d ~/local/bin ]] {
+    export PATH="$HOME/local/bin:$PATH"
+}
+
