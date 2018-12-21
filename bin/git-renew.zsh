b89aa3a8854ac36d015703cee1ee13922838f90b
A.M <andares@outlook.com>
2018-12-22 00:52:36 +0800

    fix bug

--- bin/git-renew.zsh
+++ bin/git-renew.zsh
@@ -2,10 +2,10 @@
 main() {
     local target=$1
     [[ ! $target ]] && target='.'
-    local br=`git -C $1 branch | grep '*'`;
+    local br=`git -C $target branch | grep '*'`;
     br=${br/* /}
-    git -C $1 fetch --all
-    git -C $1 reset --hard origin/${br}
+    git -C $target fetch --all
+    git -C $target reset --hard origin/${br}
 }
 
 main $*
