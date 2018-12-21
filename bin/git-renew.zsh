171b9ae70e92d2429cbc96bd553d998210db9a9c
A.M <andares@outlook.com>
2018-12-22 00:48:38 +0800

    fix bug

--- bin/git-renew.zsh
+++ bin/git-renew.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 main() {
-    git.renew $1
+    local br=`git -C $1 branch | grep '*'`;
+    br=${br/* /}
+    git -C $1 fetch --all
+    git -C $1 reset --hard origin/${br}
 }
 
 main $*
