434a3d91390f4ddb51a3f2059181d606374a4c58
A.M <andares@outlook.com>
2019-02-13 20:30:22 +0800

    update

--- init.zsh
+++ init.zsh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 
 if [[ -d /home/worker/local/bin ]] {
+    chmod +x /home/worker/local/bin/*
     export PATH="/home/worker/local/bin:$PATH"
 }
 if [[ -d ~/local/bin ]] {
+    chmod +x $HOME/local/bin/*
     export PATH="$HOME/local/bin:$PATH"
 }
 
