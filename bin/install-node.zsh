08ce29b38baacdef2a0a5ba6913e2551a9fc92b2
A.M <andares@outlook.com>
2019-03-14 21:48:24 +0800

    fix bug

--- bin/install-node.zsh
+++ bin/install-node.zsh
@@ -4,5 +4,5 @@ local name=node-v10.15.3-linux-x64
 
 wget -c https://nodejs.org/dist/latest-v10.x/${name}.tar.xz -P /tmp/
 tar xJf /tmp/${name}.tar.xz -C /tmp
-chown worker:worker -R /tmp/${name}
+sudo chown worker:worker -R /tmp/${name}
 sudo su - worker -c "mkdir ~/local;mv /tmp/${name}/* ~/local/"
