1a1a0d8fea458eb72db420b059ac263a9eb1a35f
A.M <andares@outlook.com>
2019-03-14 21:54:09 +0800

    update

--- bin/install-node.zsh
+++ bin/install-node.zsh
@@ -2,8 +2,8 @@
 
 local name=node-v10.15.3-linux-x64
 
-rm -rf /tmp/${name}
-rm -rf /tmp/${name}.tar.xz
+sudo rm -rf /tmp/${name}
+sudo rm -rf /tmp/${name}.tar.xz
 
 wget -c https://nodejs.org/dist/latest-v10.x/${name}.tar.xz -P /tmp/
 tar xJf /tmp/${name}.tar.xz -C /tmp
