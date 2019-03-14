bf41b4cce9c9286f0e70813cfd0aaccf15a7064d
A.M <andares@outlook.com>
2019-03-14 21:49:11 +0800

    add clean

--- bin/install-node.zsh
+++ bin/install-node.zsh
@@ -2,6 +2,9 @@
 
 local name=node-v10.15.3-linux-x64
 
+rm -rf /tmp/${name}
+rm -rf /tmp/${name}.tar.xz
+
 wget -c https://nodejs.org/dist/latest-v10.x/${name}.tar.xz -P /tmp/
 tar xJf /tmp/${name}.tar.xz -C /tmp
 sudo chown worker:worker -R /tmp/${name}
