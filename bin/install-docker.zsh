400088e125b14e254020e5d535b6144632bd94ae
A.M <andares@outlook.com>
2019-03-14 21:31:43 +0800

    add install-docker cmd

---
+++ bin/install-docker.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# 卸载旧版
+sudo apt remove docker docker-engine docker.io
+# 安装新版
+curl -fsSL get.docker.com -o /tmp/get-docker.sh
+sudo sh /tmp/get-docker.sh
+docker --version
+
+# 加用户进组
+sudo useradd -m worker
+sudo usermod -a -G docker worker
+
+# 安装docker-compose
+# 查看 https://docs.docker.com/compose/install/#install-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
