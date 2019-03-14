944f625dff6cada2d63c38babdfb1d73fda5deaa
A.M <andares@outlook.com>
2019-03-14 22:58:59 +0800

    update

--- bin/install-node.zsh
+++ bin/install-node.zsh
@@ -9,3 +9,6 @@ wget -c https://nodejs.org/dist/latest-v10.x/${name}.tar.xz -P /tmp/
 tar xJf /tmp/${name}.tar.xz -C /tmp
 sudo chown worker:worker -R /tmp/${name}
 sudo su - worker -c "mkdir ~/local;cp -rf /tmp/${name}/* ~/local/"
+
+echo > ~/.npmrc
+npm config set registry https://registry.npm.taobao.org
