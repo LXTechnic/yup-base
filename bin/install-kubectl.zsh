7e692cdab0a0ebbbcae217a6e4342f860d2e2980
A.M <andares@outlook.com>
2019-07-09 17:42:05 +0800

    update

--- bin/install-kubectl.zsh
+++ bin/install-kubectl.zsh
@@ -2,7 +2,7 @@
 
 local name=v1.13.8
 
-wget -c https://storage.googleapis.com/kubernetes-release/release/v1.13.8/bin/linux/amd64/kubectl -O /tmp/kubectl
+wget -c https://storage.googleapis.com/kubernetes-release/release/${name}/bin/linux/amd64/kubectl -O /tmp/kubectl
 
 touch-local
 
