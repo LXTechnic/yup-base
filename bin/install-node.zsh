273a08a299487235918ec3ec5bf0d2c829c0ed0e
A.M <andares@outlook.com>
2019-09-17 23:06:59 +0800

    update

--- bin/install-node.zsh
+++ bin/install-node.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
-local name=node-v10.15.3-linux-x64
+local name=node-v12.10.0-linux-x64
 
 sudo rm -rf /tmp/${name}
 sudo rm -rf /tmp/${name}.tar.xz
 
-wget -c https://nodejs.org/dist/latest-v10.x/${name}.tar.xz -P /tmp/
+wget -c https://nodejs.org/dist/latest-v12.x/${name}.tar.xz -P /tmp/
 tar xJf /tmp/${name}.tar.xz -C /tmp
-sudo chown worker:worker -R /tmp/${name}
-sudo su - worker -c "mkdir ~/local;cp -rf /tmp/${name}/* ~/local/"
+sudo chown root:root -R /tmp/${name}
+sudo cp -rf /tmp/${name}/* /usr/local/
 
 echo > ~/.npmrc
 npm config set registry https://registry.npm.taobao.org
