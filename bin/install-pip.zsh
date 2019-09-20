f72e26ab58574304dafea9dff5fa76d4dcc50cc9
A.M <andares@outlook.com>
2019-09-20 19:07:07 +0800

    update

--- bin/install-pip.zsh
+++ bin/install-pip.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 
-python3 "$(curl -fsSL https://bootstrap.pypa.io/get-pip.py)"
+rm /tmp/get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py
