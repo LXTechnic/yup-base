0eee9d73986f4ca95a099dc5a5ab1f4576b717f6
A.M <andares@outlook.com>
2019-09-20 19:14:37 +0800

    update

--- bin/install-pip.zsh
+++ bin/install-pip.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-if [[ -e /tmp/get-pip.py ]] {
-  rm /tmp/get-pip.py
-}
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+# if [[ -e /tmp/get-pip.py ]] {
+#   rm /tmp/get-pip.py
+# }
+wget -c https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py -o /tmp/download-get-pip.log
 python3 /tmp/get-pip.py
 rm /tmp/get-pip.py
