8cd2ace737a50bd1745f57a02621a10c3a0ab4a9
A.M <andares@outlook.com>
2019-09-22 17:30:21 +0800

    updat

--- bin/install-pip.zsh
+++ bin/install-pip.zsh
@@ -1,8 +1,14 @@
 #!/usr/bin/env zsh
 
+if [[ ! -e ~/.pip ]] {
+  mkdir ~/.pip
+  file.write ~/.pip/pip.conf '[global]'
+  file.write ~/.pip/pip.conf 'index-url = https://mirrors.aliyun.com/pypi/simple/'
+}
+
 # if [[ -e /tmp/get-pip.py ]] {
 #   rm /tmp/get-pip.py
 # }
 wget -c https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py -o /tmp/download-get-pip.log
-python3 /tmp/get-pip.py
+python /tmp/get-pip.py
 rm /tmp/get-pip.py
